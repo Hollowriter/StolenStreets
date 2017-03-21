ae13ac4fa424d00c139731eed0c8208720d5012d
benjallauro <benjallauro@gmail.com>
2017-03-21 17:48:13 -0300

    Movimiento de plataforma flotante terminado
    La plataforma flotante ya se puede mover con facilidad. Solo le falta la
    colisión. :)

--- StolenStreetsMaterials/source/sprites/PlataformaFlotante.hx
+++ StolenStreetsMaterials/source/sprites/PlataformaFlotante.hx
@@ -9,38 +9,68 @@ import flixel.util.FlxColor;
 
 class PlataformaFlotante extends FlxSprite 
 {
-	var puedeMoverse:Bool = true; // MUY IMPORTANTE. cCon esto se decide si la plataforma es movil o no.
+	var movimientoVertical:Bool = true; //Con tan solo cambiar estos bools, se puede decidir si la plataforma se mueve verticalmente o no.
+	var movimientoHorizontal = true;	//Con tan solo cambiar estos bools, se puede decidir si la plataforma se mueve horizontalmente o no.
 	var arriba:Bool = false;
-	var puntoMedio:Float = 0;
+	var izquierda:Bool = false;
+	var puntoMedioVertical:Float = 0;
+	var puntoMedioHorizontal:Float = 0;
 
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
 		makeGraphic(50, 25, FlxColor.BLUE);
-		x = 100;
+		x = 300;
 		y = 150;
-		puntoMedio = y;
+		puntoMedioVertical = y;
+		puntoMedioHorizontal = x;
 	}
-	public function moverse()
+	public function moverseVertical()
 	{
-		puedeMoverse = true;
+		movimientoVertical = true;
 	}
-	public function frenar()
+	public function frenarVertical()
 	{
-		puedeMoverse = false;
+		movimientoVertical = false;
+	}
+	public function moverseHorizontal()
+	{
+		movimientoHorizontal = true;
+	}
+	public function frenarHorizontal()
+	{
+		movimientoHorizontal = false;
 	}
 	private function chequearVuelo()
 	{
-		//CAMBIOS DE DIRECCION (ARRIBA Y ABAJO):
-		if (arriba == true && y <= puntoMedio - 50 && puedeMoverse == true)
-			arriba = false;
-		if (arriba == false && y >= puntoMedio + 50 && puedeMoverse == true)
-			arriba = true;
-		//MOVIMIENTO VERTICAL:
-		if (arriba == true && puedeMoverse == true)
-			y--;
-		else if (arriba == false && puedeMoverse == true)
-			y++;
+		//MOVIMIENTO VERTICAL
+		if (movimientoVertical == true)
+		{
+			//Cambio de direccion vertical
+			if (arriba == true && y <= puntoMedioVertical - 50 && movimientoVertical == true)
+				arriba = false;
+			if (arriba == false && y >= puntoMedioVertical + 50 && movimientoVertical == true)
+				arriba = true;
+			//Movimiento vertical
+			if (arriba == true && movimientoVertical == true)
+				y--;
+			else if (arriba == false && movimientoVertical == true)
+				y++;
+		}
+		//MOVIMIENTO HORIZONTAL
+		if (movimientoHorizontal == true)
+		{
+			//Cambio de direccion horizontal
+			if (izquierda == true && x <= puntoMedioHorizontal - 50 && movimientoHorizontal == true)
+				izquierda = false;
+			if (izquierda == false && x >= puntoMedioHorizontal + 50 && movimientoHorizontal == true)
+				izquierda = true;
+			//Movimiento horizontal
+			if (izquierda == true && movimientoHorizontal == true)
+				x--;
+			else if (izquierda == false && movimientoHorizontal == true)
+				x++;	
+		}
 	}
 	override public function update(elapsed:Float):Void
 	{

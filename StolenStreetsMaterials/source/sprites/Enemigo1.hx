3d12e7c97cc10ab192307a942533c968ef202e3d
WanderKnight-97 <micaela.pereyra@hotmail.com>
2017-03-21 16:57:30 -0300

    enemigo base

---
+++ StolenStreetsMaterials/source/sprites/Enemigo1.hx
@@ -0,0 +1,27 @@
+package sprites;
+
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import source.Reg;
+import sprites.Golpe;
+
+/**
+ * ...
+ * @author ...
+ */
+class Enemigo1 extends BaseEnemigo 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		makeGraphic(30, 30, FlxColor.BROWN);
+	}
+	override public function move(){
+		super.move();
+		
+	}
+}

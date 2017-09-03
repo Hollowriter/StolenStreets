385aed7263231174daf51a3feabb0ca8776a6190
benjallauro <benjallauro@gmail.com>
2017-09-03 02:05:20 -0300

    bug de los checkpoints reparado

--- StolenStreetsMaterials/source/states/PlayState.hx
+++ StolenStreetsMaterials/source/states/PlayState.hx
@@ -198,7 +198,7 @@ class PlayState extends FlxState{
 		//Colision entre Mili y los trampolines
 		for (i in 0...(Reg.Trampolines.members.length)){
 			if ((Reg.Players.members[0].y + (Reg.Players.members[0].height / 2)) < Reg.Trampolines.members[i].y){
-				if (FlxG.collide(Reg.Players.members[0], Reg.Trampolines.members[i])){
+				if (FlxG.overlap(Reg.Players.members[0], Reg.Trampolines.members[i])){
 					Reg.Players.members[0].SaltoTrampolin();
 				}
 		}

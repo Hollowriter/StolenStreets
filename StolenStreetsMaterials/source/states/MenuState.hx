5bec824b35b20606502d4579d51feaafe4474fec
More997 <moresfever@hotmail.com>
2017-09-17 22:41:55 -0300

    Menu temporal añadido

--- StolenStreetsMaterials/source/states/MenuState.hx
+++ StolenStreetsMaterials/source/states/MenuState.hx
@@ -8,10 +8,26 @@ import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 
 class MenuState extends FlxState{
+	private var comando:FlxText; //Toca Enter para empezar el Juego
 	override public function create():Void{
 		super.create();
+		comando = new FlxText();
+		comando.text = "PRESS ENTER TO PLAY";
+		comando.color = 0xB2FFB5;
+		comando.scale.x = 5;
+		comando.scale.y = 5;
+		comando.x = (FlxG.width / 2) - comando.scale.x;
+		comando.y = (FlxG.height / 4) - comando.scale.y;
+		comando.setBorderStyle(FlxTextBorderStyle.SHADOW, 0xff1abcc9);
+		comando.scrollFactor.set(0, 0);
+		comando.visible = true;
+		add(comando);
 	}
 	override public function update(elapsed:Float):Void{
 		super.update(elapsed);
+		if (FlxG.keys.justPressed.ENTER)
+		 {
+			 FlxG.switchState(new PlayState());
+		 }
 	}
 }

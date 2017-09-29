31930b4145641b6af4c13458674566068714c643
46 <46>
2017-09-29 16:34:08 -0300

    arte de los trampolines agregado

--- StolenStreetsMaterials/source/sprites/Trampolin.hx
+++ StolenStreetsMaterials/source/sprites/Trampolin.hx
@@ -1,6 +1,7 @@
 package sprites;
 
 import flixel.FlxSprite;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.FlxG;
 import flixel.ui.FlxButton;
@@ -10,14 +11,24 @@ import flixel.util.FlxColor;
  * ...
  * @author MorenaMontero(inicial)
  */
-class Trampolin extends FlxSprite 
-{
-
-	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
-	{
+class Trampolin extends FlxSprite {
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) {
 		super(X, Y, SimpleGraphic);
-		loadGraphic(AssetPaths.PuertayTrampolin__png, true, 70, 10);
+		loadGraphic(AssetPaths.PuertayTrampolin__png, true, 70, 30);
 		immovable = true;
+		animation.add("Titilado", [0, 1], 4, true);
+		animation.add("Aplastada", [4, 0], 5, true);
+		animation.play("Titilado");
 	}
-	
+	public function Aplastandolo(player:Jugador){
+		if (player.overlaps(this)){
+			animation.play("Aplastada");
+		}
+	}
+	override public function update(elapsed:Float){
+		super.update(elapsed);
+		if (animation.getByName("Aplastada").finished){
+			animation.play("Titilado");
+		}
+	}
 }

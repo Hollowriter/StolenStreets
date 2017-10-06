56587d94bc3deb57ea9a102cee7d8913bce60cda
411 <411>
2017-10-06 16:10:03 -0300

    fondo con parallax en proceso

---
+++ StolenStreetsMaterials/source/sprites/FondoDeNoche.hx
@@ -0,0 +1,21 @@
+package sprites;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import source.Reg;
+
+/**
+ * ...
+ * @author ...
+ */
+class FondoDeNoche extends FlxSprite{
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) {
+		super(X, Y, SimpleGraphic);
+		loadGraphic(AssetPaths.Noche__png, false, 1920, 1080);
+	}
+}
+override public function update(elapsed:Float){
+	super.update(elpased);
+	x = Reg.posXjugador;
+	y = Reg.posYjugador;
+}

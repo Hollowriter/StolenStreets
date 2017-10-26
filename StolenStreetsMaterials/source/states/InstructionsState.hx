8d43d16e0ede981bfe354fd60482f44d99a28fc8
47 <47>
2017-10-26 20:15:38 -0300

    Version final 1.3

--- StolenStreetsMaterials/source/states/InstructionsState.hx
+++ StolenStreetsMaterials/source/states/InstructionsState.hx
@@ -13,8 +13,6 @@ import source.Reg;
 
 
 class InstructionsState extends FlxState{
-	private var pulsaEnter:FlxText;
-	private var siguientePagina:FlxText;
 	private var enPagina1:Bool = true;
 	private var pagina1:FlxSprite;
 	private var pagina2:FlxSprite;
@@ -25,26 +23,9 @@ class InstructionsState extends FlxState{
 		musica.loadEmbedded(AssetPaths.break_clubstar_zone__ogg, true);
 		musica.volume = 0.1;
 		musica.play();
-		pulsaEnter = new FlxText();
-		siguientePagina = new FlxText();
 		pagina1 = new FlxSprite();
 		pagina2 = new FlxSprite();
-		
-		pulsaEnter.setFormat(AssetPaths.StolenStreet_Regular__ttf, 14);
-		pulsaEnter.text = "ENTER FOR";
-		pulsaEnter.color = 0xFFFFFFF;
-		pulsaEnter.scale.x = 2.5;
-		pulsaEnter.scale.y = 2.5;
-		pulsaEnter.x = (FlxG.width / 1.3) - pulsaEnter.scale.x;
-		pulsaEnter.y = (FlxG.height / 5.5) - pulsaEnter.scale.y;
-		
-		siguientePagina.setFormat(AssetPaths.StolenStreet_Regular__ttf, 14);
-		siguientePagina.text = "NEXT PAGE";
-		siguientePagina.color = 0xFFFFFFF;
-		siguientePagina.scale.x = 2.5;
-		siguientePagina.scale.y = 2.5;
-		siguientePagina.x = (FlxG.width / 1.3) - siguientePagina.scale.x;
-		siguientePagina.y = (FlxG.height / 4) - siguientePagina.scale.y;
+
 		if(Reg.sofiElegida == false)
 			pagina1.loadGraphic(AssetPaths.guia1mili__png, true, 940, 480);
 		else if(Reg.sofiElegida == true)
@@ -54,8 +35,7 @@ class InstructionsState extends FlxState{
 		else if(Reg.sofiElegida == true)
 			pagina2.loadGraphic(AssetPaths.guia2sofi__png, true, 940, 480);
 		add(pagina1);
-		add(pulsaEnter);
-		add(siguientePagina);
+
 	}
 	override public function update(elapsed:Float):Void{
 		super.update(elapsed);
